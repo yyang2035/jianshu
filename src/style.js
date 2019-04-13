1f9fe9f5794a2425d9262f4cfe81e59bc7bb4b83
yyang2035 <yyang2035@163.com>
2019-04-13 15:20:35 +0800

    完成header布局

---
+++ src/style.js
@@ -0,0 +1,10 @@
+
+import { createGlobalStyle } from 'styled-components'
+ 
+export const GlobalStyle = createGlobalStyle`
+    body {
+        line-height: 1;
+        margin: 0;
+        padding: 0;
+	}
+`
