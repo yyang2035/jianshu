d6b60183dabb017eabc6bed8e341364381e1674b
yyang2035 <yyang2035@163.com>
2019-04-16 20:43:56 +0800

    代码优化

--- src/common/header/store/actionCreators.js
+++ src/common/header/store/actionCreators.js
@@ -2,6 +2,13 @@ import * as constants from './constants'
 import {fromJS} from 'immutable'
 import axios from 'axios'
 
+const changeList = (data)=>{
+  return {
+    type:constants.CHANGE_LIST,
+    data:fromJS(data)
+  }
+}
+
 export const getSerchFocusAction = ()=>{
   const action = {
     type:constants.SERCH_FOCUS
@@ -16,19 +23,11 @@ export const getSerchBlurAction = ()=>{
   return action
 }
 
-const changeList = (data)=>{
-  return {
-    type:constants.CHANGE_LIST,
-    data:fromJS(data)
-  }
-}
-
 export const getList = ()=>{
   return (dispatch)=>{
     axios.get('/api/headerList.json').then((res)=>{
       let data = res.data.data;
-      console.log(data);
-      
+      // console.log(data);
       dispatch(changeList(data))
     }).catch(()=>{
       console.log('error')
--- src/common/header/store/reducer.js
+++ src/common/header/store/reducer.js
@@ -5,17 +5,29 @@ const defaultState = fromJS({ //让state变成一个immutable对象了
   list:[]    //fromJS也会包里面的list对象转为immutable数组。
 })
 export default (state = defaultState, action)=>{
-  if (action.type===constants.SERCH_FOCUS){
-//immutable对象的set方法，会结合之前immutable对象的值
-//和设置的值，返回一个全新的对象，即并不会修改原先的数据。
-    return state.set('focused',true) //因为state已经是一个immutable对象了，所以可以用.set方法进行设置。
-  }
-  if (action.type===constants.SERCH_BLUR){
-    return state.set('focused',false)
+//   if (action.type===constants.SERCH_FOCUS){
+// //immutable对象的set方法，会结合之前immutable对象的值
+// //和设置的值，返回一个全新的对象，即并不会修改原先的数据。
+//     return state.set('focused',true) //因为state已经是一个immutable对象了，所以可以用.set方法进行设置。
+//   }
+//   if (action.type===constants.SERCH_BLUR){
+//     return state.set('focused',false)
+//   }
+
+//   if(action.type===constants.CHANGE_LIST){
+//     return state.set('list',action.data) //此时的action.data是个普通的数组，数据类型就变了，肯定会出错的。(actionCreators.js中用fromJS一下data就没事了)
+//   }
+
+//多个if语句可以用switch替代：
+  switch(action.type){
+    case constants.SERCH_FOCUS:
+      return state.set('focused',true) 
+    case constants.SERCH_BLUR:
+      return state.set('focused',false)
+    case constants.CHANGE_LIST:
+      return state.set('list',action.data)
+    default:
+      return state  
   }
 
-  if(action.type===constants.CHANGE_LIST){
-    return state.set('list',action.data) //此时的action.data是个普通的数组，数据类型就变了，肯定会出错的。(actionCreators.js中用fromJS一下data就没事了)
-  }
-  return state
 }
